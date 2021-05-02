14f64382d55dcae8be07b0b5277fd4b06b26bff5
wbkearly <3207264942@qq.com>
2021-05-02 20:23:18 +0800

    Initial commit

---
+++ src/test/java/priv/wbk/springbucks/SpringBucksApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.wbk.springbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBucksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
